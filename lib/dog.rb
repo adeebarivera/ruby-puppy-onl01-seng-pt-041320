6a67cfa613cc32ebe10bcaa336441f60faa400f5
adeeba reaz <adeebarivera@outlook.com>
2020-05-07 00:41:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,15 +7,22 @@
     @name = name
     save
   end
-
+  
+  def self.all
+     @@all
+  end 
+  
   def self.clear_all
     @@all.clear
   end
+  
+  def self.print_all
+    puts @@all.map{ |dog| dog.name}
+  end
+  
 
-  def self.all
-    @@all.each do |d|
-      puts d.name
-    end
+  def save 
+    @@all << self
   end
 end
 
